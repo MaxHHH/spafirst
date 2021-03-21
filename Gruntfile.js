b890ae74fa1281e9be4134195a04c09a0d27a4dc
MaxHHH <1779665219@qq.com>
2021-03-21 15:14:51 +0800

    111

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,30 @@
 module.exports = function (grunt) {
     grunt.initConfig({
-        less:{
-            compile: {
-                files:{ 'dist/compiled.css': 'css/*.less' }
-            }
+      htmlmin: {
+        options: {
+          collapseWhitespace: true,
+          preserveLineBreaks: false
+        },
+        files: {
+          src: './index.html',
+          dest: 'dist/index.html'
         }
+      },
+      cssmin: {
+        'dist/index.css': 'index.css'
+      },
+      uglify: {
+        release:{
+          files: {
+            'dist/main.js': 'main.js';
+            'dist/unwrong.js': 'unwrong.js'
+          }
+        }
+      }
     });
-
-    grunt.loadNpmTasks('grunt-contrib-less');
-    
-    grunt.registerTask('default',['less:compile']);
-};
+  
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['htmlmin','cssmin','uglify']); 
+  };
--- main.js
+++ main.js
@@ -1,35 +1,34 @@
-$(function() {
-    var $width = $('#width'),
+$(()=>{
+    let $width = $('#width'),
         $height = $('#height'),
-        $btnCal = $('#calculate'),
+        $btnCal = $('#calc'),
         $perimeter = $('#perimeter'),
         $area = $('#area'),
         $widthValidate = $('#width-validate'),
         $heightValidate = $('#height-validate'),
         isPassValidate = false;
   
-    $forkMeGH.show('https://github.com/wangding/rectangle');
-    $bszPageFooter.show('body');
-  
-    $width.focusout(function() {
-      var result = validate($width.val());
+    $width.focusout(() => {
+      console.log($width.val());
+      let result = validate($width.val());
+      console.log(result);
       isPassValidate = result.isOK;
       if(!result.isOK) {
         $widthValidate.html('宽度' + result.reason);
-        $width.select();
+        $width.select();                        
       } else {
-        $widthValidate.html('');
+        $widthValidate.html('');                  
       }
     });
   
-    $width.keypress(function(e) {
+    $width.keypress((e) => {
       if(!isLegalKey(e.key, e.target.value, e.target.selectionStart)) {
-        e.preventDefault();
+        e.preventDefault();          
       }
     });
-  
-    $height.focusout(function() {
-      var result = validate($height.val());
+    
+    $height.focusout(() => {
+      let result = validate($height.val());
       isPassValidate = result.isOK;
       if(!result.isOK) {
         $heightValidate.html('高度' + result.reason);
@@ -41,19 +40,20 @@ $(function() {
   
     $height.keypress(function(e) {
       if(!isLegalKey(e.key, e.target.value, e.target.selectionStart)) {
-        e.preventDefault();
-      }
+        e.preventDefault();           
+      }   
     });
   
-    $btnCal.click(function(){
+    $btnCal.click(() => {
       if(!isPassValidate) return;
-  
-      var w = $width.val(),
-          h = $height.val();
-  
-      var r = new Rectangle(w, h);
-  
-      $perimeter.val(r.perimeter());
-      $area.val(r.area());
+      let w = Number($width.val()),
+          h = Number($height.val());
+      function l1(x,n){
+          return Math.round(x * Math.pow(10,n)) / Math.pow(10,n);         
+      }
+      console.log(w,h);
+      $perimeter.val(l1(2 * (w + h), 2));
+      $area.val(l1(w * h, 2));          
     });
   });
+  
