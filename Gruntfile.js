012671a31c0c33c01599179dba31779856beaddf
MaxHHH <1779665219@qq.com>
2021-03-21 14:59:57 +0800

    1

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,13 @@
 module.exports = function (grunt) {
     grunt.initConfig({
-      htmlmin: {
-        options: {
-          collapseWhitespace: true,
-          preserveLineBreaks: false
-        },
-        files: {
-          src: './index.html',
-          dest: 'dist/index.html'
+        less:{
+            compile: {
+                files:{ 'dist/compiled.css': 'css/*.less' }
+            }
         }
-      },
-      cssmin: {
-        'dist/index.css': 'index.css'
-      },
-      uglify: {
-        release:{
-          files: {
-            'dist/index.js': 'index.js';
-          }
-        }
-      }
     });
-  
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.registerTask('default', ['htmlmin','cssmin','uglify']); 
-  };
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    
+    grunt.registerTask('default',['less:compile']);
+};
